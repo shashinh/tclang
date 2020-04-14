4c08a019dcca712f6f9214b289eb28877d031193
shashinh <shashin.halalingaiah@gmail.com>
2020-04-14 14:09:33 +0530

    Added tokenizer to the reader

--- reader/reader.rkt
+++ reader/reader.rkt
@@ -3,7 +3,17 @@
 
 (define (read-syntax path port)
   (define parse-tree (parse path (make-tokenizer port)))
-  (define module-datum `(module tclang-mod "expander.rkt"
+  (define module-datum `(module tclang-mod "../expander/expander.rkt"
                           ,parse-tree))
   (datum->syntax #f module-datum))
 (provide read-syntax)
+
+(require brag/support)
+(define (make-tokenizer port)
+  (define (next-token)
+    (define bf-lexer
+      (lexer
+       [(char-set "><-.,+[]") lexeme]
+       [any-char (next-token)]))
+    (bf-lexer port))  
+  next-token)
